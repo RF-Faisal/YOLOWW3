7bec555c53aecce0a36ae579148cc9a7cefa32c7
M Rayhan Ferdous Faisal <202014022@student.mist.ac.bd>
2022-01-08 22:27:15 +0600

    8 Jan 2022

---
+++ Achievements/Achievements.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        // sticky _navbar on scroll script
+        if (this.scrollY > 20) {
+            $('._navbar').addClass("sticky");
+        } else {
+            $('._navbar').removeClass("sticky");
+        }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('._navbar .menu li a').click(function() {
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/_navbar script
+    $('.menu-btn').click(function() {
+        $('._navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
